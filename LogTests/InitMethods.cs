5cbb1d793b645f11b913c5287383b55efa883ccc
max1219 <bescenniy.2003@mail.ru>
2023-11-20 12:26:56 +0300

    Added c#12 constructor to InitMethods

--- LogTests/InitMethods.cs
+++ LogTests/InitMethods.cs
@@ -3,22 +3,12 @@ using System.Reflection;
 
 namespace LogTests;
 
-public class InitMethods
+public class InitMethods(IEnumerable<MethodBase>? beforeAll, IEnumerable<MethodBase>? afterAll, IEnumerable<MethodBase>? beforeEach, IEnumerable<MethodBase>? afterEach, IEnumerable<MethodBase>? beforeEachTime, IEnumerable<MethodBase>? afterEachTime)
 {
-    public IEnumerable<MethodBase>? BeforeAll;
-    public IEnumerable<MethodBase>? AfterAll;
-    public IEnumerable<MethodBase>? BeforeEach;
-    public IEnumerable<MethodBase>? AfterEach;
-    public IEnumerable<MethodBase>? BeforeEachTime;
-    public IEnumerable<MethodBase>? AfterEachTime;
-
-    public InitMethods(IEnumerable<MethodBase>? beforeAll, IEnumerable<MethodBase>? afterAll, IEnumerable<MethodBase>? beforeEach, IEnumerable<MethodBase>? afterEach, IEnumerable<MethodBase>? beforeEachTime, IEnumerable<MethodBase>? afterEachTime)
-    {
-        BeforeAll = beforeAll;
-        AfterAll = afterAll;
-        BeforeEach = beforeEach;
-        AfterEach = afterEach;
-        BeforeEachTime = beforeEachTime;
-        AfterEachTime = afterEachTime;
-    }
+    public IEnumerable<MethodBase>? BeforeAll = beforeAll;
+    public IEnumerable<MethodBase>? AfterAll = afterAll;
+    public IEnumerable<MethodBase>? BeforeEach = beforeEach;
+    public IEnumerable<MethodBase>? AfterEach = afterEach;
+    public IEnumerable<MethodBase>? BeforeEachTime = beforeEachTime;
+    public IEnumerable<MethodBase>? AfterEachTime = afterEachTime;
 }
