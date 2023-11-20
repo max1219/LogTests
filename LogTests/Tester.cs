8db78d8528647db06dc01b2e4648087f4fd88932
max1219 <bescenniy.2003@mail.ru>
2023-11-20 09:14:43 +0300

    Added Tester

---
+++ LogTests/Tester.cs
@@ -0,0 +1,34 @@
+﻿using LogTests.Operators;
+
+namespace LogTests;
+
+public class Tester
+{
+    private readonly LinkedList<(string? result, IOperator @operator)> _results;
+
+    internal Tester()
+    {
+        _results = new LinkedList<(string? result, IOperator @operator)>();
+    }
+
+    internal bool IsAllSuccess()
+    {
+        return _results.All(tuple => tuple.result is null);
+    }
+
+    internal IEnumerable<(string? result, IOperator @operator)> GetWrong()
+    {
+        return _results.Where(tuple => tuple.result is not null);
+    }
+
+    public void Check(IOperator @operator)
+    {
+        string? result = @operator.Check();
+        _results.AddLast((result, @operator));
+    }
+
+    internal void Clear()
+    {
+        _results.Clear();
+    }
+}
