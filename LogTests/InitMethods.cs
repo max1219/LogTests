0529283b226c51a3ef576914bb6855fac7e92158
max1219 <bescenniy.2003@mail.ru>
2023-11-20 10:01:48 +0300

    Added InitMethods

---
+++ LogTests/InitMethods.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace LogTests;
+
+public class InitMethods
+{
+    public IEnumerable<MethodBase>? BeforeAll;
+    public IEnumerable<MethodBase>? AfterAll;
+    public IEnumerable<MethodBase>? BeforeEach;
+    public IEnumerable<MethodBase>? AfterEach;
+    public IEnumerable<MethodBase>? BeforeEachTime;
+    public IEnumerable<MethodBase>? AfterEachTime;
+
+    public InitMethods(IEnumerable<MethodBase>? beforeAll, IEnumerable<MethodBase>? afterAll, IEnumerable<MethodBase>? beforeEach, IEnumerable<MethodBase>? afterEach, IEnumerable<MethodBase>? beforeEachTime, IEnumerable<MethodBase>? afterEachTime)
+    {
+        BeforeAll = beforeAll;
+        AfterAll = afterAll;
+        BeforeEach = beforeEach;
+        AfterEach = afterEach;
+        BeforeEachTime = beforeEachTime;
+        AfterEachTime = afterEachTime;
+    }
+}
